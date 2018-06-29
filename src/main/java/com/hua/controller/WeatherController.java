d0a7dc64dbd48515826c37eed6f44edcb6e2ac09
Harvey <harvey0379@163.com>
2018-06-29 18:29:14 +0800

    complete weather api

--- src/main/java/com/hua/controller/WeatherController.java
+++ src/main/java/com/hua/controller/WeatherController.java
@@ -12,8 +12,14 @@ public class WeatherController {
     public JSONObject getJson() {
         JSONObject jsonObject = new JSONObject();
         Weather weather = new Weather();
-        weather.setId("1");
-        weather.setName("Beijing");
+        weather.setTemp1("-2");
+        weather.setTemp2("16");
+        weather.setCity("Beijing");
+        weather.setCityid("101010100");
+        weather.setWeather("sunny");
+        weather.setImg1("n0.gif");
+        weather.setImg2("d0.gif");
+        weather.setPtime("18:00");
         jsonObject.put("weatherinfo", weather);
         return jsonObject;
     }
